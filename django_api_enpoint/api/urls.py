ae90dd772cf3b49e3314934762d15d02e212e739
joanico <joanico@catalpa.io>
2022-02-02 22:24:32 +0900

    modified view context data

--- django_api_enpoint/api/urls.py
+++ django_api_enpoint/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
-from .views import GetKoboApi, ApiView
+from .views import GetKoboApi
 
 urlpatterns = [
-    path('', GetKoboApi.as_view(template_name="api/kobo_list.html"), name='koboapi'),
-    path('api/', ApiView.as_view(), name='api'),
+    path('', GetKoboApi,  name='aplikante'),
 ]
--- django_api_enpoint/api/views.py
+++ django_api_enpoint/api/views.py
@@ -9,18 +9,9 @@ import requests
 import coreapi
 import io
 
-class ApiView(TemplateView):
-    template_name =  'api/api_endpoint.html'
-    def get_context_data(self, *args, **kwargs):
-        context = super(ApiView, self).get_context_data(*args, **kwargs)
-        context['all_beneficiary'] = Beneficiary.objects.all().order_by('-id')
-        return context
-
-class GetKoboApi(TemplateView):
-    template_name = 'api/kobo_list.html'
-    def get_context_data(self, *args, **kwargs):
-        context = {
-            'koboapi' : get_apis(),
-        }
-        return context
-
+def GetKoboApi(request):
+    aplikante = Beneficiary.objects.all().order_by('date')
+    context = {
+        'aplikante': aplikante
+    }
+    return render(request, "api/kobo_list.html", context)
